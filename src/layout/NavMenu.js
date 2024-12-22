5c410cf13c0f9664c8efa0f2703c465ea358f9f0
thsvo <155262940+thsvo@users.noreply.github.com>
2024-12-22 22:06:14 +0600

    menu

--- src/layout/NavMenu.js
+++ src/layout/NavMenu.js
@@ -1,38 +1,39 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function NavigationMenu() {
   const [activeItem, setActiveItem] = useState('OFFER')
 
   const menuItems = [
-    'OFFER',
-    'Organic Foods',
-    'Spices/Masala',
-    'Beauty Products',
-    'Fashion',
-    'Gadget -Device',
-    'Electronics',
+    { label: 'OFFER', link: '/shop' },
+    { label: 'Organic Foods', link: '/shop' },
+    { label: 'খেজুর গুড়', link: '/shop?category=%E0%A6%96%E0%A7%87%E0%A6%9C%E0%A7%81%E0%A6%B0-%E0%A6%97%E0%A7%81%E0%A6%A1%E0%A6%BC' },
+    { label: 'সরিষার তেল', link: '/shop?category=সরিষার-তেল' },
+    { label: 'Ghee (ঘি)', link: '/shop?category=ghee-(%E0%A6%98%E0%A6%BF)' },
+    { label: 'Honey', link: '/shop?category=honey' },
   ]
 
   return (
     <nav className="navigation center">
       <ul className="navigation__list">
         {menuItems.map((item) => (
-          <li 
-            key={item} 
-            className={`navigation__item ${activeItem === item ? 'navigation__item--active' : ''}`}
+          <li
+            key={item.label}
+            className={`navigation__item ${activeItem === item.label ? 'navigation__item--active' : ''}`}
           >
-            <button 
-              onClick={() => setActiveItem(item)}
+            <Link 
+              href={item.link} 
+              passHref
               className="navigation__button"
+              onClick={() => setActiveItem(item.label)}
             >
-              {item}
-            </button>
+              {item.label}
+            </Link>
           </li>
         ))}
       </ul>
     </nav>
   )
 }
-
